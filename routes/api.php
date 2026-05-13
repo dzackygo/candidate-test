79fdb18a04aee14e23807d639e36a8f3d52e5fb0
Dzacky Ilham <dzackyilham04@gmail.com>
2026-05-13 18:36:29 +0800

    chore: clean api route imports

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
