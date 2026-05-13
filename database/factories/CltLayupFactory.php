88f664cee1a24de5460ee0af757bd9cd74662283
Dzacky Ilham <dzackyilham04@gmail.com>
2026-05-13 18:36:43 +0800

    test: add clt layup factory

---
+++ database/factories/CltLayupFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Supplier;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CltLayup>
+ */
+class CltLayupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'supplier_id' => Supplier::factory(),
+            'name' => fake()->unique()->words(3, true),
+        ];
+    }
+}
