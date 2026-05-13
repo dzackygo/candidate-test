fc99c69ddb77fb71b4e3ac4705e4ea3a29be9149
Dzacky Ilham <dzackyilham04@gmail.com>
2026-05-13 18:36:36 +0800

    feat: add supplier create request validation

---
+++ app/Http/Requests/StoreSupplierRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSupplierRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()?->can('create', \App\Models\Supplier::class) ?? false;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique:suppliers,name'],
+        ];
+    }
+}
